9868f9b4261edb36c18e9be6dc696998cebdae19
Rizki Dafa Naldi <rizkidafa0404@gmail.com>
2026-08-23 16:42:26 +0700

    perbaikan

--- src/components/admin/ui/customselected.jsx
+++ src/components/admin/ui/customselected.jsx
@@ -28,27 +28,37 @@ export default function CustomSelect({
       {/* Trigger */}
       <button
         type="button"
-        onClick={() => setOpen(!open)}
-        className={`
-          flex w-full items-center justify-between text-white cursor-pointer
-          rounded-xl border px-4 py-3 text-left
-          transition-all duration-300
-          ${
-            error
-              ? "border-red-500/50"
-              : "border-white/10 hover:border-amber-500/40"
+        onClick={() => {
+          const nextOpen = !open;
+
+          setOpen(nextOpen);
+
+          if (nextOpen) {
+            setTimeout(() => {
+              wrapperRef.current?.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+              });
+            }, 50);
           }
-          bg-white/4
-        `}
+        }}
+        className={`
+        flex w-full items-center justify-between text-white cursor-pointer
+        rounded-xl border px-4 py-3 text-left
+        transition-all duration-300
+        ${error ? "border-red-500/50" : "border-white/10 hover:border-amber-500/40"}
+        bg-white/4
+      `}
       >
         <span className={value ? "text-white" : "text-slate-500"}>
           {value || placeholder}
         </span>
 
         <FaChevronDown
-          className={`transition-transform duration-300 ${
-            open ? "rotate-180" : ""
-          }`}
+          className={`
+          transition-all duration-300
+          ${open ? "rotate-180 text-amber-400" : "text-slate-400"}
+        `}
         />
       </button>
 
@@ -56,12 +66,14 @@ export default function CustomSelect({
       {open && (
         <div
           className="
-            absolute z-50 mt-2 w-full
-            overflow-hidden rounded-xl
-            border border-white/10
-            bg-emerald-950
-            shadow-2xl 
-          "
+          absolute z-50 mt-2 w-full
+          overflow-hidden rounded-xl
+          border border-white/10
+          bg-emerald-950
+          shadow-2xl
+          origin-top
+          animate-[dropdown_0.25s_ease-out]
+        "
         >
           {options.map((item) => (
             <button
